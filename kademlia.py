db38b1cabe14d6f188a25b7c8d19551de717ea90
galvanix <elia@dl1ye.com>
2018-04-21 22:36:13 +0200

    implemented basic peer rpc functionality

--- kademlia.py
+++ kademlia.py
@@ -2,7 +2,40 @@
 
 import asyncio,json
 from collections import deque
-import time
+import time,random
+
+'''
+Messages passed between nodes are simply Python dictionaries that are
+serialized to JSON.
+
+Every RPC request contains the following fields:
+
+    rpc: string, possible values are "PING", "FIND_NODE", "FIND_VALUE", "STORE"
+    type: "REQ"
+    echo: random 160 bit value encoded as hex string, needs to be echoed back in reply
+
+    Additional fields for PING: None
+    Additional fields for FIND_NODE: id: string, 160 bit node id as hex string
+    Additional fields for FIND_VALUE: key: string, 160 bit key as hex string
+    Additional fields for STORE: key: string, 160 bit key as hex string
+                                 val: string, 160 bit key as hex string
+
+Every RPC reply contains the following fields:
+
+    rpc and echo as above
+
+    type: "REP"
+
+    Additional fields for PING: None
+    Additional fields for FIND_NODE: nodes: list of tuples with ("address",port) pairs
+
+    Additional fields for FIND_VALUE: if key/val not on this node:
+                                        nodes: list of tuples with ("address",port) pairs
+                                      else:
+                                          value: the stored value
+
+    Additional fields for STORE: None
+'''
 
 # this sublass implements the transmission of RPC requsts to other Kademlia nodes
 class KademliaRPCProtocol(asyncio.DatagramProtocol):
@@ -43,29 +76,114 @@ class KademliaListenProtocol(asyncio.DatagramProtocol):
 # class that stores peer information such as ID, address and alive status
 # which is important for updating the routing table
 class KademliaPeer:
-    def __init__(self,id,addr,alive=True):
+    def __init__(self,id,addr,event_loop,alive=True,timeout=5):
         self.id = id
         self.addr = addr
+        self.event_loop = event_loop
         self.alive = alive
+        # timeout after which the peer is considered dead after sending a request
+        self.timeout = 5
+
+        # this property is set by the KademliaNode class when it finds a response
+        # from this peer in the incoming message queue
+        self.response = None
+        # the expected random echo value
+        self.expected_echo = None
+
+    # launch a generic request
+    async def generic_request(self,req):
+
+        conn = self.event_loop.create_datagram_endpoint(
+            lambda: KademliaRPCProtocol(req),
+            local_addr=self.addr,
+            reuse_port=True)
+
+        await self.event_loop.ensure_future(conn)
+
+        t_start = time.now()
+
+        while self.response == None or self.response.echo != self.expected_echo:
+            while (time.now() - t_start) < self.timeout:
+                yield
+            else:
+                return None
+        else:
+            return self.response
 
     # ping this node to see if it is still alive
     async def ping(self):
-        pass
+        self.expected_echo = hex(random.randint(0,2**160-1))
+
+        req = { "rpc": "PING", "type": "REQ",
+                "echo": self.expected_echo}
+
+        await rep = self.generic_request(req)
+
+        # if the reply is a None object then the node timed out or responded
+        # with an incorrect echo
+        self.alive = rep != None
+
+        self.response = None
+        return
 
     # send a find_node rpc to this node
     async def find_node(self,id):
-        pass
+        self.expected_echo = hex(random.randint(0,2**160-1))
+
+        req = { "rpc": "FIND_NODE", "type": "REQ",
+                "echo": self.expected_echo,
+                "id": id}
+
+        await rep = self.generic_request(req)
+
+        if rep != None:
+            tmp = self.response.nodes
+            self.response = None
+            return tmp
+        else:
+            return None
 
     # send a find_value rpc to this node
-    async def find_value(self,value):
-        await self.find_node(id)
+    async def find_value(self,key):
+        self.expected_echo = hex(random.randint(0,2**160-1))
+
+        req = { "rpc": "FIND_VALUE", "type": "REQ",
+                "echo": self.expected_echo,
+                "key": key}
+
+        await rep = self.generic_request(req)
+
+        if rep != None:
+
+            try:
+                tmp = self.response.value
+            except KeyError:
+                tmp = self.response.nodes
+            finally:
+                self.response = None
+
+            return tmp
+
+        else:
+            return None
 
     # send a store RPC to this node
     async def store(self,key,value):
-        pass
+        self.expected_echo = hex(random.randint(0,2**160-1))
+
+        req = { "rpc": "STORE", "type": "REQ",
+                "echo": self.expected_echo,
+                "key": key,
+                "value": value}
+
+        await rep = self.generic_request(req)
+
+        self.response = None
+        return
 
 class KademliaKBucket:
-    def __init__(self,bucket_size=20):
+    def __init__(self,event_loop,bucket_size=20):
+        self.event_loop = event_loop
         # size of one k-bucket in the routing table
         self.bucket_size = bucket_size
         # initialize k buckets
@@ -111,6 +229,10 @@ class KademliaKBucket:
             else:
                 self.kbuckets[bucket_index].append(peer)
 
+        # calculate the Kademlia distance metric given two id strings
+        def distance(self,id1,id2):
+            return int(id1,16) ^ int(id2,16)
+
 class KademliaNode:
     def __init__(self,id,addr,event_loop,bucket_size=20,concurrency=3):
         self.id = id
@@ -139,33 +261,19 @@ class KademliaNode:
     async def bootstrap(self):
         pass
 
-    # work on entries in the request queue
+    # work on the incoming message queue
     async def process_message_queue(self):
         pass
 
-    # timer function that will ensure that the routing table will be refreshed
-    # once every hour
-    async def refresh_timer(self):
-        while True:
-            asyncio.sleep(3600)
-            # TODO: kick off routing table refresh
-
-    # calculate the Kademlia distance metric given two id strings
-    def distance(self,id1,id2):
-        return int(id1,16) ^ int(id2,16)
-
 if __name__ == "__main__":
-
-    request_queue = deque()
-    reply_queue = deque()
 
     loop = asyncio.get_event_loop()
 
     try:
-        t = loop.create_datagram_endpoint(
-                lambda: KademliaListenProtocol(request_queue,reply_queue),
-                local_addr=("127.0.0.1",1234))
-        loop.run_until_complete(t)
+        kn = KademliaNode(id='2ef7bde608ce5404e97d5f042f95f89f1c232871',
+                          addr=('127.0.0.1',5000),
+                          event_loop=loop)
+        loop.run_until_complete(kn.bootstrap())
         loop.run_forever()
 
     finally:
